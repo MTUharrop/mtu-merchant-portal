f2e8b542da39d147173b573d5cb411998cb30564
harropda <harropda@gmail.com>
2022-12-14 15:12:44 +0000

    Final settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +27,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        Password::defaults(function() {
+            return Password::min(size:16)
+            ->letters()
+            ->numbers()
+            ->symbols()
+            ->mixedcase()
+            ->uncompromised();
+        });
     }
 }
